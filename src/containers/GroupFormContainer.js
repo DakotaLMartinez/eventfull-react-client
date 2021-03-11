99d36c854c2ea493feb05243dd18b9e62ff21878
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-03-10 18:33:25 -0800

    fix ring around field

--- src/containers/GroupFormContainer.js
+++ src/containers/GroupFormContainer.js
@@ -29,7 +29,10 @@ class GroupFormContainer extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6">
+      <form
+        onSubmit={this.handleSubmit}
+        className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6"
+      >
         <h1 className="text-center text-3xl font-semibold mb-2">New Group</h1>
         <fieldset>
           <p className="h-8 pl-4 text-red-400">{this.state.errors.name}</p>
@@ -39,10 +42,17 @@ class GroupFormContainer extends Component {
             onChange={this.handleChange}
             value={this.state.name}
             placeholder="Name your group"
-            className="w-full border p-4 mb-4"
+            className={`w-full border-2 focus:outline-none focus:ring-2 p-4 mb-4 ${
+              this.state.errors.name && "focus:ring-red-400"
+            }`}
           />
         </fieldset>
-        <button className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200" type="submit">Add Group</button>
+        <button
+          className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200"
+          type="submit"
+        >
+          Add Group
+        </button>
       </form>
     );
   }
